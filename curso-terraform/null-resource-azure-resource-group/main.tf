bb2cf242eb3079c89e5601edee9cbfeb7370c3f6
Gustavo Costa <gustavo.costa@linker.com.br>
2023-04-03 10:54:20 -0300

    updates in curso terraform

---
+++ curso-terraform/null-resource-azure-resource-group/main.tf
@@ -0,0 +1,23 @@
+terraform {
+
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.44.1"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "storage_account_resource_group"
+    storage_account_name = "gustavocostaremotestate"
+    container_name       = "remote-state"
+    key                  = "azure-resource-group-null-resource/terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  features {}
+}
